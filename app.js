ff5aa5fcbd26b3f9400147d7ef6d998491925441
JashanBansal0708 <jashanbansal7856@yahoo.com>
2018-11-18 23:33:34 +0530

    Keep the contact page working with validations of express-validator

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var expressValidator = require('express-validator');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -16,6 +18,9 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(expressValidator())
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,33 @@ router.get('/about', function(req,res,next) {
   res.render('about', { title: 'Platform to share the code'})
 })
 
+
+router.route('/contact')
+  .get(function(req,res,next) {
+    res.render('contact', {
+      name: '',
+      email : '',
+      message : '',
+      error: ''        
+  })
+  })
+  .post(function(req,res,next){
+    req.checkBody('name','name should not be empty').notEmpty();
+    req.checkBody('email','invalid email').isEmail();
+    req.checkBody('message','message should not be empty').notEmpty();
+    var errors = req.validationErrors();
+    
+    if(errors){
+      res.render('contact', {
+          name: req.body.name,
+          email : req.body.email,
+          message : req.body.message,
+          error: errors        
+      });
+    }
+    else{
+      res.render('thank');
+    }
+  });
+
 module.exports = router;
