791e1edb29533121c85b7cb3ea15bdb4becf9232
JashanBansal0708 <jashanbansal7856@yahoo.com>
2018-11-21 12:11:04 +0530

    Delete the users.js file. Create login and register pages

--- routes/index.js
+++ routes/index.js
@@ -54,4 +54,12 @@ router.route('/contact')
     }
   });
 
+router.get('/login', function(req,res,next){
+  res.render('login');
+});
+
+router.get('/register', function(req,res,next){
+  res.render('register');
+})
+
 module.exports = router;
