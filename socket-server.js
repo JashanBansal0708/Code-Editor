ed277b59758fc2c38d653fd1f7c575f6a130ea6a
jashanbansal0708 <jashanbansal7856@yahoo.com>
2018-11-26 13:09:15 +0530

    Add the basic level chat functionality. There is a BUG as messages send to all tasks

---
+++ socket-server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var socketIO = require('socket.io');
+
+
+module.exports = function(server){
+    var io = socketIO(server);
+    io.on('connection', function(socket){
+        socket.on('chatMessage', function(data){
+            io.emit('chatMessage', data);
+        })
+    })
+}
