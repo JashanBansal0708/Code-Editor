9506952d3e76e19c0bdceda0eb6d95ff3d620026
jashanbansal0708 <jashanbansal7856@yahoo.com>
2018-11-27 14:04:09 +0530

    Complete the collaboration of text editor. Not saved in database till now

--- socket-server.js
+++ socket-server.js
@@ -1,21 +1,36 @@
 'use strict';
 
+var ot = require('ot');
 var socketIO = require('socket.io');
-
+var roomList = {}
 
 module.exports = function(server){
-    var io = socketIO(server);
-    io.on('connection', function(socket){
-        socket.on('joinRoom', function(data){
-            socket.room = data.room;
-            socket.join(data.room)
-        })
-        socket.on('chatMessage', function(data){
-            io.to(socket.room).emit('chatMessage', data);
-        })
+  var str = 'THis is a markdown heading \n\n' +
+            'var i = i+1;';
 
-        socket.on('disconnect', function(){
-            socket.leave(socket.room);
-        })
-    })
-}
+  var io = socketIO(server);
+  io.on('connection',function(socket){
+    socket.on('joinRoom', function(data){
+      if(!roomList[data.room]){
+        var socketIOServer = new ot.EditorSocketIOServer(str, [], data.room, function(socket,cb){
+          var self = this;
+          Task.findByIdAndUpdate(data.room, {content: self.document}, function(err){
+            if(err) return cb(false);
+            return cb(true);
+          });
+        });
+        roomList[data.room] = socketIOServer;
+      }
+      roomList[data.room].addClient(socket);
+      roomList[data.room].setName(socket, data.username);
+      socket.room = data.room;
+      socket.join(data.room);
+    });
+    socket.on('chatMessage',function(data){
+      io.to(socket.room).emit('chatMessage',data);
+    });
+    socket.on('disconnect',function(){
+      socket.leave(socket.room);
+    });
+  })
+};
